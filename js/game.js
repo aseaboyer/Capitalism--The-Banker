011193bf5e16ba895d162841889ec38271a686be
aseaboyer <aseaboyer@gmail.com>
2014-06-22 01:31:49 -0700

    added time vals to game object

--- js/game.js
+++ js/game.js
@@ -11,11 +11,53 @@ var game = {
 	context: {},
 	phase: "menu",
 	phases: [ "load game", "menu", "load level", "play", "win", "lost" ], // for ref. @aseaboyer
+	time: {
+		last: 0,
+		current: 0,
+		delta: 0,
+		update: function() {
+			var d = new Date();
+			if(this.last == 0) {
+				this.last = d.getTime()
+			} else {
+				this.last = this.current;
+			}
+			this.current = d.getTime();
+			this.delta = this.current - this.last;
+		},
+	},
 	init: function(canvas) {
 		this.canvas = canvas;
 		this.context = canvas.getContext("2d");
 		
+		this.time.update();
+		this.time.update();
+		
 		this.update();
+	},
+	update: function() {
+		this.time.update();
+	},
+	draw: function(context) {
+		context.clearRect(0, 0, this.context.canvas.height, this.context.canvas.width);
+		
+		// draw background and things here
+	},
+	cursor: {
+		x: 0,
+		y: 0,
+		size: 25,
+		draw: function(context) {
+			context.fillStyle = "#00ffff";
+			context.fillRect(this.x - (this.size*0.5), this.y - (this.size*0.5), this.size, this.size);
+		},
+	},
+};
+
+var player = {
+	loc: {
+		x: 0,
+		y: 15,
 	},
 	update: function() {
 
@@ -26,28 +68,11 @@ var game = {
 		}
 		
 	},
-	draw: function(context) {
-		context.clearRect(0, 0, this.context.canvas.height, this.context.canvas.width);
-		
-		context.fillStyle = "#ff00ff";
-		context.fillRect(player.loc.x, player.loc.y, 25, 25);
-	},
-	cursor: {
-		x: 0,
-		y: 0,
-		draw: function(context) {
-			context.fillStyle = "#00ffff";
-			context.fillRect(this.x, this.y, 25, 25);
-		},
-	},
-};
-
-var player = {
-	loc: {
-		x: 0,
-		y: 15,
-	},
 	speed: 5,
+	draw: function(context) {
+		context.fillStyle = "#ff00ff";
+		context.fillRect(this.loc.x, this.loc.y, 25, 25);
+	},
 };
 
 function start() {
@@ -56,7 +81,11 @@ function start() {
 }
 function animate() {
     game.update();
+    player.update();
+	
     game.draw(context);
+	
+    player.draw(context);
 	
     game.cursor.draw(context);
 	
